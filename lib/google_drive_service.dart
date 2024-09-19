71413b552fd57544d6007f6263f4db2ee8e7e5ea
FelipeAraujoOliveira <felipearaujooliveirade@gmail.com>
2024-09-18 22:52:55 -0300

    permissions

--- lib/google_drive_service.dart
+++ lib/google_drive_service.dart
@@ -2,7 +2,6 @@ import 'package:googleapis/drive/v3.dart' as drive;
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:http/http.dart' as http;
 import 'package:http/io_client.dart';
-import 'package:mime/mime.dart';
 import 'dart:io'; // Importar a classe File para manipulação de arquivos locais
 
 class GoogleDriveService {
